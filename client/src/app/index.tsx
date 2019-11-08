c4a1346202d0d92f083c07e07a34d385eb0275b5
Robin Schuster <kontakt@robinschuster.net>
2019-11-08 20:40:05 +0100

    init client

---
+++ client/src/app/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const rootEl = document.getElementById('app');
+
+ReactDOM.render(
+    <div>react loaded :)</div>,
+    rootEl
+);
