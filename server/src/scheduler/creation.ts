a6e1a20d18f6c8c2b65ad432c352f32199c02eef
paqe <4215065+paqe@users.noreply.github.com>
2019-11-09 19:14:40 +0100

    Expose scheduler

--- server/src/scheduler/creation.ts
+++ server/src/scheduler/creation.ts
@@ -1,34 +1,34 @@
+import { Gathering } from '../../../shared/types'
+
 export const randomSchedule = ({
   timeSlots,
   rooms,
   topics,
 }: Gathering): Schedule => {
-  const timeSlotSessions: Schedule["timeSlotSessions"] = timeSlots.map(
-    () => [],
-  );
+  const timeSlotSessions: Schedule['timeSlotSessions'] = timeSlots.map(() => [])
 
-  const shuffledTopics = Array.from(topics).sort(() => Math.random() - 0.5);
+  const shuffledTopics = Array.from(topics).sort(() => Math.random() - 0.5)
 
   for (const topic of shuffledTopics) {
     // TODO: prefer adding to time slot with fewest sessions
     const firstAvailableTimeSlotIndex = timeSlotSessions.findIndex(
       timeSlot => timeSlot.length < rooms.length,
-    );
-    const scheduleIsFull = firstAvailableTimeSlotIndex === -1;
+    )
+    const scheduleIsFull = firstAvailableTimeSlotIndex === -1
     if (scheduleIsFull) {
-      break;
+      break
     }
-    const sessions = timeSlotSessions[firstAvailableTimeSlotIndex];
-    const numExistingSessions = sessions.length;
-    const nextRoom = rooms[numExistingSessions];
+    const sessions = timeSlotSessions[firstAvailableTimeSlotIndex]
+    const numExistingSessions = sessions.length
+    const nextRoom = rooms[numExistingSessions]
     const session = {
       topicId: topic.id,
       roomId: nextRoom.id,
-    };
-    timeSlotSessions[firstAvailableTimeSlotIndex].push(session);
+    }
+    timeSlotSessions[firstAvailableTimeSlotIndex].push(session)
   }
   const schedule = {
     timeSlotSessions,
-  };
-  return schedule;
-};
+  }
+  return schedule
+}
---
+++ server/src/scheduler/usage.ts
@@ -0,0 +1,5 @@
+import { gathering } from '../../../shared/randomGathering'
+
+import { evolveSchedule } from './'
+
+console.log(evolveSchedule(gathering))
