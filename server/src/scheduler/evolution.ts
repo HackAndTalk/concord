88561e120358cb07d01e6915c83949e73ecf83fc
paqe <4215065+paqe@users.noreply.github.com>
2019-11-09 19:14:40 +0100

    Expose scheduler

--- server/src/scheduler/evolution.ts
+++ server/src/scheduler/evolution.ts
@@ -1,40 +1,59 @@
-import { randomSchedule } from "./creation";
-import { penalty } from "./evaluation";
-import { swapSessions } from "./mutation";
+import { randomSchedule } from './creation'
+import { penalty } from './evaluation'
+import { swapSessions } from './mutation'
+import { Gathering, Topic } from '../../../shared/types'
 
-import { gathering } from "../../../shared/randomGathering";
+import { gathering } from '../../../shared/randomGathering'
 
-const populationSize = 100;
-const numParents = 50;
+const populationSize = 100
+const numParents = 50
 
-const timeoutMs = 5 * 1000;
-const start = Date.now();
-const end = start + timeoutMs;
+const timeoutMs = 5 * 1000
+const start = Date.now()
+const end = start + timeoutMs
 
 const evaluateSchedule = (schedule: Schedule) => {
-  return { schedule, penalty: penalty(schedule, gathering) };
-};
+  return { schedule, penalty: penalty(schedule, gathering) }
+}
 
-let population = Array.from({ length: populationSize }, () =>
-  randomSchedule(gathering),
-);
-do {
-  const evaluatedPopulation = population
-    .map(evaluateSchedule)
-    .sort((a, b) => a.penalty - b.penalty);
-  console.log(
-    evaluatedPopulation.map(evaluatedSchedule => evaluatedSchedule.penalty),
-  );
-  const parents = evaluatedPopulation
-    .slice(0, numParents)
-    .map(({ schedule }) => schedule);
-  const children = parents.flatMap(parent =>
-    Array.from({ length: 9 }, () => swapSessions(parent)),
-  );
-  population = [...parents, ...children];
-} while (Date.now() < end);
+type TimeSlotTopics = {
+  startTime: Date
+  endTime: Date
+  topics: Topic[]
+}
 
-const bestSchedule = population[0];
-const bestSchedulePenalty = evaluateSchedule(bestSchedule);
-console.log(`Best schedule (penalty: ${bestSchedulePenalty})
-${JSON.stringify(bestSchedule, null, 2)}`);
+export const evolveSchedule = (gathering: Gathering): TimeSlotTopics[] => {
+  let population = Array.from({ length: populationSize }, () =>
+    randomSchedule(gathering),
+  )
+  do {
+    const evaluatedPopulation = population
+      .map(evaluateSchedule)
+      .sort((a, b) => a.penalty - b.penalty)
+    const parents = evaluatedPopulation
+      .slice(0, numParents)
+      .map(({ schedule }) => schedule)
+    const children = parents.flatMap(parent =>
+      Array.from({ length: 9 }, () => swapSessions(parent)),
+    )
+    population = [...parents, ...children]
+  } while (Date.now() < end)
+
+  const bestSchedule = population[0]
+
+  const { timeSlots, topics } = gathering
+  const scheduleTimeSlots = bestSchedule.timeSlotSessions.map(
+    (sessions, index) => {
+      const timeSlot = timeSlots[index]
+      return {
+        startTime: timeSlot.startTime,
+        endTime: timeSlot.endTime,
+        topics: sessions.flatMap(session =>
+          topics.filter(topic => topic.id === session.topicId),
+        ),
+      }
+    },
+  )
+
+  return scheduleTimeSlots
+}
--- server/src/scheduler/mutation.ts
+++ server/src/scheduler/mutation.ts
@@ -1,32 +1,34 @@
-const randomIndex = (array: unknown[]): number =>
-  Math.floor(Math.random() * array.length);
+import { Gathering } from '../../../shared/types'
 
-const randomChoice = <T>(array: T[]): T => array[randomIndex(array)];
+const randomIndex = (array: unknown[]): number =>
+  Math.floor(Math.random() * array.length)
+
+const randomChoice = <T>(array: T[]): T => array[randomIndex(array)]
 
 export const swapSessions = (schedule: Schedule): Schedule => {
-  const { timeSlotSessions } = schedule;
+  const { timeSlotSessions } = schedule
 
   // Select two distinct random time slots
-  let timeSlot1Index: number;
-  let timeSlot2Index: number;
-  let timeSlot1;
-  let timeSlot2;
+  let timeSlot1Index: number
+  let timeSlot2Index: number
+  let timeSlot1
+  let timeSlot2
   do {
-    timeSlot1Index = randomIndex(timeSlotSessions);
-    timeSlot2Index = randomIndex(timeSlotSessions);
-    timeSlot1 = timeSlotSessions[timeSlot1Index];
-    timeSlot2 = timeSlotSessions[timeSlot2Index];
+    timeSlot1Index = randomIndex(timeSlotSessions)
+    timeSlot2Index = randomIndex(timeSlotSessions)
+    timeSlot1 = timeSlotSessions[timeSlot1Index]
+    timeSlot2 = timeSlotSessions[timeSlot2Index]
   } while (
     timeSlot1Index === timeSlot2Index ||
     timeSlot1.length === 0 ||
     timeSlot2.length === 0
-  );
+  )
 
   // Select a session from each time slot
-  const session1Index = randomIndex(timeSlot1);
-  const session2Index = randomIndex(timeSlot2);
-  const topic1Id = timeSlotSessions[timeSlot1Index][session1Index].topicId;
-  const topic2Id = timeSlotSessions[timeSlot1Index][session1Index].topicId;
+  const session1Index = randomIndex(timeSlot1)
+  const session2Index = randomIndex(timeSlot2)
+  const topic1Id = timeSlotSessions[timeSlot1Index][session1Index].topicId
+  const topic2Id = timeSlotSessions[timeSlot1Index][session1Index].topicId
 
   // Create schedule with swapped topics
   const newSchedule = {
@@ -37,18 +39,18 @@ export const swapSessions = (schedule: Schedule): Schedule => {
           timeSlotIndex === timeSlot1Index &&
           sessionIndex === session1Index
         ) {
-          return { ...session, topicId: topic2Id };
+          return { ...session, topicId: topic2Id }
         }
         if (
           timeSlotIndex === timeSlot2Index &&
           sessionIndex === session2Index
         ) {
-          return { ...session, topicId: topic1Id };
+          return { ...session, topicId: topic1Id }
         }
-        return session;
+        return session
       }),
     ),
-  };
+  }
 
-  return newSchedule;
-};
+  return newSchedule
+}
