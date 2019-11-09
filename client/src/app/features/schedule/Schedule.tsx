11fb3fa7892afe31c646b89033588f7e8e669089
paqe <4215065+paqe@users.noreply.github.com>
2019-11-09 21:33:37 +0100

    Add simple schedule screen with example data

--- client/src/app/features/schedule/Schedule.tsx
+++ client/src/app/features/schedule/Schedule.tsx
@@ -1,17 +1,71 @@
-import * as React from 'react';
-import FixedFabPageContainer from '../../components/FixedFabPageContainer';
-import PageTitle from '../../components/PageTitle';
+import * as React from 'react'
+import moment from 'moment'
+import FixedFabPageContainer from '../../components/FixedFabPageContainer'
+import PageTitle from '../../components/PageTitle'
+import TableRow, { TableRowProps } from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { Table, TableBody } from '@material-ui/core'
 
-const schedule = []
+const exampleSchedule = [
+  {
+    startTime: new Date('2019-11-09T12:00:00Z'),
+    endTime: new Date('2019-11-09T14:00:00Z'),
+    topics: [
+      {
+        id: '1',
+        title: 'Title',
+        description: 'Description',
+        moderator: '2',
+        voterIds: ['4'],
+      },
+      {
+        id: '2',
+        title: 'Title 2',
+        description: 'Description 2',
+        moderator: '4',
+        voterIds: ['5', '10'],
+      },
+    ],
+  },
+  {
+    startTime: new Date('2019-11-09T14:00:00Z'),
+    endTime: new Date('2019-11-09T16:00:00Z'),
+    topics: [
+      {
+        id: '1',
+        title: 'Title',
+        description: 'Description',
+        moderator: '2',
+        voterIds: ['4'],
+      },
+      {
+        id: '2',
+        title: 'Title 2',
+        description: 'Description 2',
+        moderator: '4',
+        voterIds: ['5', '10'],
+      },
+    ],
+  },
+]
 
 const Schedule: React.FC = props => {
-    return (
-        <FixedFabPageContainer>
-            <PageTitle variant='h6'>
-                Schedule
-            </PageTitle>
-        </FixedFabPageContainer>
-    )
+  return (
+    <FixedFabPageContainer>
+      <PageTitle variant="h6">Schedule</PageTitle>
+      <Table>
+        <TableBody>
+          {exampleSchedule.map(timeSlot => (
+            <TableRow key={String(timeSlot.startTime)}>
+              <TableCell>{moment(timeSlot.startTime).format('LT')}</TableCell>
+              <TableCell>{timeSlot.topics[0].title}</TableCell>
+              <TableCell>{timeSlot.topics[0].description}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </FixedFabPageContainer>
+  )
 }
 
-export default Schedule;
+export default Schedule
